c76d7dd90cb462fb9dba5d468cac6ef4c88f86b0
Ruslan <sergeikashtan1337@gmail.com>
2026-04-29 19:42:00 +0200

    a little of optimization

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"LbpBGSyVOFU5rSWqXsnE2QzTgR212TtkyFFLe+sNnmk=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
